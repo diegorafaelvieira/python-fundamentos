fcbec433fe353d33957d9305c7e540eabd8875d8
Diego Rafael Vieira <diegorafaelvieira@bol.com.br>
2025-06-01 23:22:12 -0300

    Adicionando novas aulas

---
+++ teste3.py
@@ -0,0 +1,7 @@
+number1 = int("10")
+number2 = 5
+sum = number1 + number2 
+print(sum)
+
+name = str(10)
+print(type(name))
---
+++ teste4.py
@@ -0,0 +1,7 @@
+name = input("Qual seu nome? ")
+age = int(input("Qual sua idade? "))
+
+print("-------------")
+print("Nome: " + name)
+print("Idade: " + str(age))
+print("O seu nome é " + name + " e você tem " + str(age) + " anos.")
