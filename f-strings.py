be2fd736b87950d0a3f2109c280a508a7b844cfc
Diego Rafael Vieira <diegorafaelvieira@bol.com.br>
2025-06-05 17:16:57 -0300

    Add Aula F Strings e métodos

---
+++ f-strings.py
@@ -0,0 +1,5 @@
+name = input("Qual o seu nome? ").title()
+age = int(input("Qual a sua idade? "))
+
+print("------")
+print(f"O seu nome é {name} ({len(name)} caracteres) e você tem {age + 1} anos.")
