63cc94cd1205265ce7823b50450045c49354bc21
Diego Rafael Vieira <diegorafaelvieira@bol.com.br>
2025-06-09 15:43:16 -0300

    Add Aula Condicionais

---
+++ condicionais.py
@@ -0,0 +1,14 @@
+idade = int(input("Qual a sua idade: "))
+
+if idade >= 18:
+  print("É maior de idade")
+else:
+  print("É menor de idade")
+
+
+if idade == 17:
+  print("É hora de se alistar")
+elif idade < 17:
+  print("Cedo demais...")
+else:
+  print("Tarde demais...")  
