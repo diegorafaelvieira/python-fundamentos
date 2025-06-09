e16ede2f11f55e90db5730b027d61b5bbb46e670
Diego Rafael Vieira <diegorafaelvieira@bol.com.br>
2025-06-09 15:24:29 -0300

    Add comentários

--- f-strings.py
+++ f-strings.py
@@ -1,5 +1,6 @@
 name = input("Qual o seu nome? ").title()
 age = int(input("Qual a sua idade? "))
-
+# Aqui é um comentário em uma linha 
+# VS Code: Selecionar e ctrl + /
 print("------")
 print(f"O seu nome é {name} ({len(name)} caracteres) e você tem {age + 1} anos.")
