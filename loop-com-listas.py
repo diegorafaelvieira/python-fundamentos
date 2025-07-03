1b1cb2cdf5b276baa5e8c3ce1e9de62aa8b861c2
Diego Rafael Vieira <diegorafaelvieira@bol.com.br>
2025-07-02 23:16:03 -0300

    Add Aula Controle de loops com Listas

---
+++ loop-com-listas.py
@@ -0,0 +1,19 @@
+lista = ["joão", "pedro", "bonieky", "maria"]
+
+for nome in lista:
+  print(nome)
+  
+
+
+lista2 = ["arroz", "feijão", "carne", "refrigerante"]
+
+print("Lista da feira:")
+for item in lista2:
+  print(f"Item da lista: {item.title()}")
+print("Compre tudo!")
+
+
+lista3 = [1, 5, 7, 2, 10]
+
+for numero in lista3:
+  print(f"{numero} x 2 = {numero * 2}")
