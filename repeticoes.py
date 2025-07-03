ba49247c93700281103d4a9f96c33dd9f08c242a
Diego Rafael Vieira <diegorafaelvieira@bol.com.br>
2025-07-02 23:01:28 -0300

    Add Aula Repetições

---
+++ repeticoes.py
@@ -0,0 +1,16 @@
+# While quando não se sabe quantas vezes vai executar 
+contador = 1
+while contador < 10:
+  print(contador)
+  contador += 1
+
+
+# For quando sabe quantas vezes vai executar  
+for numero in range(5):
+  print(numero)
+
+for numero in range(5, 10): # Exibe do 5 até 9
+  print(numero)
+
+# Range
+print(range(5)) # vai do 0,1,2,3,4
